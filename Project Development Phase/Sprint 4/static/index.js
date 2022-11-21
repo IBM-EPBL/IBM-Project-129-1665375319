ab3e0372379dea5f940c087e35cecf403bec2659
SakthivelS2023 <88278639+SakthivelS2023@users.noreply.github.com>
2022-11-21 18:19:39 +0530

    Delete Project Development Phase/Sprint 4 directory

--- Project Development Phase/Sprint 4/static/index.js
+++
@@ -1,9 +0,0 @@
-let menu = document.getElementById("menu");
-let links = document.getElementById("nav_links");
-let body = document.getElementById("body");
-
-menu.addEventListener("click", ()=>{
-    links.classList.toggle("nav_responsive");
-    links.classList.toggle("navbac");
-    body.classList.toggle("bacdrop");
-});
